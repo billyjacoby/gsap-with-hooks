c87da7ab3b89e3bd5762849f5a5124d1fa201841
William Jacoby <billyjacoby@gmail.com>
2019-04-20 19:25:21 -0400

    Updates

---
+++ precache-manifest.051714c539b98448399fb9a5757bc4f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4f6489f380cf6e47c25b",
+    "url": "/gsap-with-hooks/static/css/main.dd23a06f.chunk.css"
+  },
+  {
+    "revision": "4f6489f380cf6e47c25b",
+    "url": "/gsap-with-hooks/static/js/main.2b601566.chunk.js"
+  },
+  {
+    "revision": "128cb754222d0b33ff15",
+    "url": "/gsap-with-hooks/static/js/runtime~main.7fdc0ae9.js"
+  },
+  {
+    "revision": "288dc48efe70733fa45d",
+    "url": "/gsap-with-hooks/static/js/2.d63836dd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gsap-with-hooks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ad20b6148795dd53c88aeb02ffee6860",
+    "url": "/gsap-with-hooks/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "//billyjacoby.github.io/gsap-with-hooks/precache-manifest.f4da8a18e17fec211337295b986ab1dd.js"
+  "/gsap-with-hooks/precache-manifest.051714c539b98448399fb9a5757bc4f7.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("//billyjacoby.github.io/gsap-with-hooks/index.html", {
+workbox.routing.registerNavigationRoute("/gsap-with-hooks/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
