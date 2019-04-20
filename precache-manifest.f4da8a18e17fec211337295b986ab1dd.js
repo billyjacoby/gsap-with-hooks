722353281688d772121f34daa1bbfd8efaea5f3f
William Jacoby <billyjacoby@gmail.com>
2019-04-20 19:24:45 -0400

    Updates

---
+++ precache-manifest.f4da8a18e17fec211337295b986ab1dd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bf3bd50cca72e626f3fa",
+    "url": "//billyjacoby.github.io/gsap-with-hooks/static/css/main.dd23a06f.chunk.css"
+  },
+  {
+    "revision": "bf3bd50cca72e626f3fa",
+    "url": "//billyjacoby.github.io/gsap-with-hooks/static/js/main.6f5c7866.chunk.js"
+  },
+  {
+    "revision": "a3802fd1ac36cb285ce6",
+    "url": "//billyjacoby.github.io/gsap-with-hooks/static/js/runtime~main.872902f6.js"
+  },
+  {
+    "revision": "288dc48efe70733fa45d",
+    "url": "//billyjacoby.github.io/gsap-with-hooks/static/js/2.d63836dd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//billyjacoby.github.io/gsap-with-hooks/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1433f94b7e08f32c6ff9886339422e5d",
+    "url": "//billyjacoby.github.io/gsap-with-hooks/index.html"
+  }
+];
